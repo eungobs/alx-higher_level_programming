196693eb9cc11ca2494f269a6bd4114e1131dc6a
eungobs <eungobs@gmail.com>
2023-08-10 11:55:37 -0700

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!user/bin/python3
+# calculator_1.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
