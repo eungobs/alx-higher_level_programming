3faa823719a78fd67f16ce9bdb9d9a543dffa746
eungobs <eungobs@gmail.com>
2023-08-14 13:22:48 -0700

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -4,22 +4,7 @@
 * print_python_list_info - Prints basic info about Python lists.
 * @p: A PyObject list.
 */
-
-<<<<<<< HEAD
 void print_python_list_info(PyObject *p)
-=======
-<<<<<<< HEAD
-/**
-* print_python_list_info - Prints basic info about Python lists.
-* @p: A PyObject list.
-*/
-
-void print_python_list_info(PyObject *p)
-=======
-void print_python_list_info(PyObject *p);
->>>>>>> 28d65fe447305dfdae639c63a4f73abec69dad0b
->>>>>>> 45fb2f01a550cc27fd4bf0b55c8bcbe224fc6bbb
->>>>>>> 95df4ff4b569c61913dc8149926383b53da0156e
 {
 int size, alloc, i;
 PyObject *obj;
