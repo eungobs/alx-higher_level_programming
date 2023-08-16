b01faf4f26292552bcf1ade1bcb90c85081d7f66
eungobs <eungobs@gmail.com>
2023-08-16 15:39:23 +0000

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-main.py
+++ 0x04-python-more_data_structures/0-main.py
@@ -1,4 +1,6 @@
-quare_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
+#!/usr/bin/env python3
+
+square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 
 matrix = [
     [1, 2, 3],
@@ -6,6 +8,6 @@ matrix = [
     [7, 8, 9]
 ]
 
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
+result = square_matrix_simple(matrix)
+for row in result:
+    print(row)
