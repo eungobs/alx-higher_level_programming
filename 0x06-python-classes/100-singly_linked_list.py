4cb665c3e9b2ac65f4180c98422f74168799dc1e
eungobs <eungobs@gmail.com>
2023-08-29 12:00:42 -0700

    100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,53 +1,65 @@
-#!/usr/bin/python3
-"""Singly-linked list generation
+#!/usr/bin/bash/python3
+"""Square generation
 """
 
-from node import Node
+
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self._data = value
+
+    @property
+    def next_node(self):
+        return self._next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self._next_node = value
 
 
 class SinglyLinkedList:
-    """Class defined for singly linked list.
-
-    Attributes:
-        __head (Node): head of linked list
-
-    """
-
     def __init__(self):
-        """Attributes:
-            __head (Node): head of linked list
-
-        """
-        self.__head = None
+        self.head = None
 
     def sorted_insert(self, value):
-        """Inserts a new Node into the correct sorted position in the list.
-
-        Args:
-            value (int): value of new Node to be inserted into list
-
-        """
         new_node = Node(value)
-        if self.__head is None or self.__head.data >= value:
-            new_node.next_node = self.__head
-            self.__head = new_node
+
+        if self.head is None or self.head.data > value:
+            new_node.next_node = self.head
+            self.head = new_node
         else:
-            current = self.__head
+            current = self.head
             while current.next_node is not None and current.next_node.data < value:
                 current = current.next_node
             new_node.next_node = current.next_node
             current.next_node = new_node
 
     def __str__(self):
-        """Returns a string representation of the linked list.
-
-        Returns:
-            str: string representation of linked list with one node number by line
-
-        """
-        current = self.__head
         nodes = []
-        while current is not None:
+        current = self.head
+        while current:
             nodes.append(str(current.data))
             current = current.next_node
         return '\n'.join(nodes)
+
+
+if __name__ == "__main__":
+    linked_list = SinglyLinkedList()
+    linked_list.sorted_insert(5)
+    linked_list.sorted_insert(2)
+    linked_list.sorted_insert(8)
+    linked_list.sorted_insert(1)
+    linked_list.sorted_insert(7)
+    print(linked_list)
