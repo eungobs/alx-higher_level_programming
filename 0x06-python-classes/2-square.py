32b2a2924d3dd59b3dca33d101eef01bd90464a4
eungobs <eungobs@gmail.com>
2023-08-29 09:19:28 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-"""Defines a class Square."""
+"""Class square generation
+"""
+
 
 class Square:
-    """Represents a square."""
+    """Class defined for square generation.
+
+    Attributes:
+        __size (int): length of one side of square
+
+    """
 
     def __init__(self, size=0):
-        """Initialize a new Square.
+        """Args:
+            size (int): length of one side of square
 
-        Args:
-            size (int): The size of the new square.
+        Attributes:
+            __size (int): length of one side of square
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
         self.__size = size
-
-    @property
-    def size(self):
-        """Getter method for __size attribute."""
-        return self.__size
-
-if __name__ == "__main__":
-    square = Square(5)
-    print(square.size)
