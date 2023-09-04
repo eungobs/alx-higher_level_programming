3e326ec395c6ea3742df8a1345a426748f2fe5b8
eungobs <eungobs@gmail.com>
2023-09-04 15:15:57 -0700

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
 """
 Module finds all solutions for N queens problem.
+
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard.
+
+Example:
+    $ ./101-nqueens.py N
+
+N must be an integer greater than or equal to 4.
+
+Attributes:
+    board (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+
+Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen must be placed on the chessboard.
 """
 
 import sys
+
 
 
 def init_board(n):
