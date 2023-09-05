9604fe146e468d1c058eacac31b8f8f030b57395
eungobs <eungobs@gmail.com>
2023-09-05 09:09:19 -0700

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""
-Write a class LockedClass with no class or object attribute
-"""
+"""Defines a locked class."""
 
 
 class LockedClass:
-    def __setattr__(self, name, value):
-        if name == 'first_name':
-            super().__setattr__(name, value)
-        else:
-            raise AttributeError("You cannot create new instance attributes for LockedClass")
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
 
-locked_obj = LockedClass()
-locked_obj.first_name = "John"
-print(locked_obj.first_name)
-
-locked_obj.last_name = "Doe"
+    __slots__ = ["first_name"]
