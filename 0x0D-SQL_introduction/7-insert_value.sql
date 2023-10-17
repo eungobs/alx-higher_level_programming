86a8998d7288252cc8ed43d031c194ef08bfc6d7
hectoriabandini <johnyREx@gmail.com>
2023-10-17 15:53:57 -0700

    mysql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- Insert a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School";
+-- Insert to a table
+INSERT INTO `first_table` (`id`, `name`) VALUES ('89', "BEST SCHOOL");
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,2 @@
--- Displays the number of records with id = 89 in the table first_table.
-SELECT COUN(*)
-FROM `first_table`
-WHERE `id` = 89;
+-- Counts the number of entries
+SELECT COUNT(*) FROM first_table WHERE id = 89;
