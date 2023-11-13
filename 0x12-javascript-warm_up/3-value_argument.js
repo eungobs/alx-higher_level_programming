5d9d5db5e1cf18c76f1f1214f3fd6171235c6368
eungobs <eungobs@gmail.com>
2023-11-13 09:05:33 -0800

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-
-const args = process.argv.slice(2);
-const firstArg = args[0];
-console.log(firstArg ? firstArg : "No argument");
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' :process.argv[2]);
