8b4c4f42392b75a8821def3a67f380d079cc8a6f
eungobs <eungobs@gmail.com>
2023-11-13 08:21:36 -0800

    102-add_me_maybe.js

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,13 +1,4 @@
 #!/usr/bin/node
-
-function incrementAndCall(number, theFunction) {
-  number++;
-  theFunction(number);
-}
-
-// Example usage:
-function exampleFunction(value) {
-  console.log("Incremented value:", value);
-}
-
-incrementAndCall(5, exampleFunction);
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(++number);
+};
