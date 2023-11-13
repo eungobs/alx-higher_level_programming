b6a2568d2a2ea29c9b1209638a5a1c1b9e83860e
eungobs <eungobs@gmail.com>
2023-11-13 08:39:21 -0800

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-
-function factorial(n) {
-  if (isNaN(n) || n < 0) {
-    return 1;
-  }
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  return n * factorial(n - 1);
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-const arg = parseInt(process.argv[2]);
-console.log(factorial(arg));
+console.log(factorial(parseInt(process.argv[2])));
