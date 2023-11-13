3de8199d89eafa80e2baf8901a3d137dcd28e902
eungobs <eungobs@gmail.com>
2023-11-13 08:48:44 -0800

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
-const arg = parseInt(process.argv[2]);
-if (isNaN(arg)) {
-  console.log("Missing number of occurrences");
-} else {
-  for (let i = 0; i < arg; i++) {
-    console.log("C is fun");
+const num = Math.floor(Number(process.argv[2]));
+if (!isNaN(num)) {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
   }
+} else {
+  console.log('Missing number of occurrences');
 }
