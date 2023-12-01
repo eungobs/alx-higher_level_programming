6bac8c941f57786482e1f3418599bfcca3e95972
eungobs <eungobs@gmail.com>
2023-12-01 14:37:39 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
-Script that fetches https://intranet.hbtn.io/status
-"""
-import urllib.request
-import urllib.error
 
-def main():
-    """
-    Function to print a response of a specific URL
-    """
-    url = 'https://intranet.hbtn.io/status'
-    try:
-        with urllib.request.urlopen(url) as response:
-            html = response.read()
-            print('Body response:')
-            print(' - type: {}'.format(type(html)))
-            print(' - content: {}'.format(html))
-            print(' - utf8 content: {}'.format(html.decode('utf-8')))
-    except urllib.error.URLError as e:
-        print('Error fetching the URL:', e)
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print(" - type: {}".format(type(content)))
+        print(" - content: {}".format(content))
+        print(" - utf8 content: {}".format(content.decode('utf-8')))
